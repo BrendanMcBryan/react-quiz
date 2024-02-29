012db7f0770c01488e8670f4b2fab07421801333
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-02-29 15:05:06 -0800

    useReducer funtion

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -3,6 +3,10 @@ import { useReducer, useState } from 'react';
 function reducer(state, action) {
   console.log(state, action);
 
+  if (action.type === 'inc') return state + 1;
+  if (action.type === 'dec') return state - 1;
+  if (action.type === 'setCount') return action.payload;
+
   return state + action;
 }
 
@@ -14,21 +18,22 @@ function DateCounter() {
 
   // This mutates the date object.
   const date = new Date('june 21 2027');
-  date.setDate(date.getDate() + count); 
+  date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch(-1);
+    dispatch({ type: 'dec' });
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
   };
 
   const inc = function () {
-    dispatch(1);
+    dispatch({ type: 'inc' });
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
+    dispatch({ type: 'setCount', payload: Number(e.target.value) });
     // setCount(Number(e.target.value));
   };
 
